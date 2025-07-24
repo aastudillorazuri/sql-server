828fb5c1008d3c62aece1f48098b8ade1971e87d
Antonio Astudillo <aastudillorazuri@gmail.com>
2025-07-24 03:52:37 -0500

    Last update: 07/24/2025

---
+++ 01_sublenguaje_ddl/00_comment_code.sql
@@ -0,0 +1,14 @@
+/*
+En SQL, los comentarios se pueden hacer de dos maneras:
+
+Con -- [texto] (doble guión medio) para comentarios de una línea
+
+o
+
+/* [texto] */ para comentario de múltiples líneas.
+*/
+
+-- Esto es un comentario de una sola línea
+
+/* Este es un comentario
+de múltiples líneas */
