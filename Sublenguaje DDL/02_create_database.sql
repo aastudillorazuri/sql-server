fab67cbfe5149b20e46b0e027056a8cc439950ae
Antonio Astudillo <aastudillorazuri@gmail.com>
2025-08-01 01:20:45 -0500

    Last update: 08/01/2025

--- Sublenguaje DDL/02_create_database.sql
+++ Sublenguaje DDL/02_create_database.sql
@@ -10,8 +10,8 @@ CREATE DATABASE CarreraDA;
 Nos debería aparecer el mensaje: 'Commands completed successfully' ('Los comandos se han 
 completado correctamente').
 
-Antes de crear una tabla, asegúrate que estés usando la base de datos correcta. Esto puedes 
-realizarlo a través de la sentencia:
+Antes de crear una tabla o esquema, asegúrate que estés usando la base de datos correcta.
+Esto puedes realizarlo a través de la sentencia:
 */
 
 USE CarreraDA;
