4236b52a7f0c95b75363607157bc8d593b8b5702
Antonio Astudillo <aastudillorazuri@gmail.com>
2025-08-01 01:02:24 -0500

    Last update: 08/01/2025

---
+++ Sublenguaje DDL/02_create.sql
@@ -0,0 +1,17 @@
+/*
+Es utilizada para crear nuevos objetos.
+
+Crearemos una base de datos llamada ‘CarreraDA’:
+*/
+
+CREATE DATABASE CarreraDA;
+
+/*
+Nos debería aparecer el mensaje: 'Commands completed successfully' ('Los comandos se han 
+completado correctamente').
+
+Antes de crear una tabla, asegúrate que estés usando la base de datos correcta. Esto puedes 
+realizarlo a través de la sentencia:
+*/
+
+USE CarreraDA;
